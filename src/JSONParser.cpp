f63cd984adc6630b1ddba015efd75254ea11ac43
Robert Osfield <robert.osfield@gmail.com>
2025-04-14 17:39:06 +0100

    Initial experiment with parsing GLTF accessors.

---
+++ src/JSONParser.cpp
@@ -0,0 +1,298 @@
+/* <editor-fold desc="MIT License">
+
+Copyright(c) 2025 Robert Osfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+</editor-fold> */
+
+#include <vsg/core/Objects.h>
+#include <vsg/core/Value.h>
+#include <vsg/io/Path.h>
+//#include <vsg/io/json.h>
+#include <vsg/io/mem_stream.h>
+
+#include "JSONParser.h"
+
+#include <fstream>
+
+using namespace vsg2;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ObjectSchema
+//
+void ObjectSchema::read_array(JSONParser& parser, const std::string_view& name)
+{
+}
+
+void ObjectSchema::read_object(JSONParser& parser, const std::string_view& name)
+{
+}
+
+void ObjectSchema::read_string(JSONParser& parser, const std::string_view& name)
+{
+}
+
+void ObjectSchema::read_number(JSONParser& parser, const std::string_view& name, std::istream& input)
+{
+}
+
+void ObjectSchema::read_bool(JSONParser& parser, const std::string_view& name, bool value)
+{
+}
+
+void ObjectSchema::read_null(JSONParser& parser, const std::string_view& name)
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// ArraySchema
+//
+void ArraySchema::read_array(JSONParser& parser)
+{
+}
+
+void ArraySchema::read_object(JSONParser& parser)
+{
+}
+
+void ArraySchema::read_string(JSONParser& parser)
+{
+}
+
+void ArraySchema::read_number(JSONParser& parser, std::istream& input)
+{
+}
+
+void ArraySchema::read_bool(JSONParser& parser, bool value)
+{
+}
+
+void ArraySchema::read_null(JSONParser& parser)
+{
+    vsg::warn("gltf parsing error, position = ", parser.pos);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// json parser
+//
+JSONParser::JSONParser() :
+    mstr(nullptr, 0)
+{
+}
+
+bool JSONParser::read_string(std::string& value)
+{
+    if (buffer[pos] != '"') return false;
+
+    // read string
+    auto end_of_value = buffer.find('"', pos + 1);
+    if (end_of_value == std::string::npos) return false;
+
+    // could have escape characters.
+
+    value = buffer.substr(pos + 1, end_of_value - pos - 1);
+
+    pos = end_of_value + 1;
+
+    return true;
+}
+
+
+void JSONParser::read_object(ObjectSchema& schema)
+{
+    if (pos == std::string::npos) return;
+    if (buffer[pos] != '{') return;
+
+    // buffer[pos] == '{'
+    // advance past open bracket
+    pos = buffer.find_first_not_of(" \t\r\n", pos + 1);
+    if (pos == std::string::npos) return;
+
+    indent += 4;
+
+    while (pos != std::string::npos && pos < buffer.size() && buffer[pos] != '}')
+    {
+        if (buffer[pos] == '"')
+        {
+            auto end_of_string = buffer.find('"', pos + 1);
+            if (end_of_string == std::string::npos) break;
+
+            std::string_view name(&buffer[pos + 1], end_of_string - pos - 1);
+
+            // skip white space
+            pos = buffer.find_first_not_of(" \t\r\n", end_of_string + 1);
+            if (pos == std::string::npos)
+            {
+                vsg::info(indent, "read_object()  deliminator error end of buffer.");
+                break;
+            }
+
+            // make sure next charater is the {name : value} deliminator
+            if (buffer[pos] != ':')
+            {
+                vsg::info(indent, "read_object()  deliminator error buffer[", pos, "] = ", buffer[pos]);
+                break;
+            }
+
+            // skip white space
+            pos = buffer.find_first_not_of(" \t\r\n", pos + 1);
+            if (pos == std::string::npos)
+            {
+                break;
+            }
+
+            // now look to pair with value after " : "
+            if (buffer[pos] == '{')
+            {
+                schema.read_object(*this, name);
+            }
+            else if (buffer[pos] == '[')
+            {
+                schema.read_array(*this, name);
+            }
+            else if (buffer[pos] == '"')
+            {
+                schema.read_string(*this, name);
+            }
+            else
+            {
+                auto end_of_field = buffer.find_first_of(",}]", pos + 1);
+                if (end_of_field == std::string::npos) break;
+
+                auto end_of_value = end_of_field - 1;
+                while (end_of_value > 0 && white_space(buffer[end_of_value])) --end_of_value;
+
+                if (buffer.compare(pos, end_of_value - pos, "null") == 0)
+                {
+                    schema.read_null(*this, name);
+                }
+                else if (buffer.compare(pos, end_of_value - pos, "true") == 0)
+                {
+                    schema.read_bool(*this, name, true);
+                }
+                else if (buffer.compare(pos, end_of_value - pos, "false") == 0)
+                {
+                    schema.read_bool(*this, name, false);
+                }
+                else
+                {
+                    mstr.set(reinterpret_cast<const uint8_t*>(&buffer.at(pos)), end_of_value - pos + 1);
+                    schema.read_number(*this, name, mstr);
+                }
+
+                // skip to end of field
+                pos = end_of_field;
+            }
+        }
+        else if (buffer[pos] == ',')
+        {
+            ++pos;
+        }
+        else
+        {
+            vsg::info(indent, "read_object() buffer[", pos, "] = ", buffer[pos]);
+        }
+
+        pos = buffer.find_first_not_of(" \t\r\n", pos);
+    }
+
+    if (pos < buffer.size() && buffer[pos] == '}')
+    {
+        ++pos;
+    }
+
+    indent -= 4;
+}
+
+void JSONParser::read_array(ArraySchema& schema)
+{
+    pos = buffer.find_first_not_of(" \t\r\n", pos);
+    if (pos == std::string::npos) return;
+    if (buffer[pos] != '[')
+    {
+        vsg::info(indent, "read_array() could not match opening [");
+        return;
+    }
+
+    // buffer[pos] == '['
+    // advance past open bracket
+    pos = buffer.find_first_not_of(" \t\r\n", pos + 1);
+    if (pos == std::string::npos)
+    {
+        vsg::info(indent, "read_array() contents after [");
+        return;
+    }
+
+    indent += 4;
+
+    while (pos != std::string::npos && pos < buffer.size() && buffer[pos] != ']')
+    {
+        // now look to pair with value after " : "
+        if (buffer[pos] == '{')
+        {
+            schema.read_object(*this);
+        }
+        else if (buffer[pos] == '[')
+        {
+            schema.read_array(*this);
+        }
+        else if (buffer[pos] == '"')
+        {
+            if (std::string value; read_string(value))
+            {
+                schema.read_string(*this);
+            }
+        }
+        else if (buffer[pos] == ',')
+        {
+            ++pos;
+        }
+        else
+        {
+            auto end_of_field = buffer.find_first_of(",}]", pos + 1);
+            if (end_of_field == std::string::npos) break;
+
+            auto end_of_value = end_of_field - 1;
+            while (end_of_value > 0 && white_space(buffer[end_of_value])) --end_of_value;
+
+            if (buffer.compare(pos, end_of_value - pos, "null") == 0)
+            {
+                schema.read_null(*this);
+            }
+            else if (buffer.compare(pos, end_of_value - pos, "true") == 0)
+            {
+                schema.read_bool(*this, true);
+            }
+            else if (buffer.compare(pos, end_of_value - pos, "false") == 0)
+            {
+                schema.read_bool(*this, false);
+            }
+            else
+            {
+                mstr.set(reinterpret_cast<const uint8_t*>(&buffer.at(pos)), end_of_value - pos + 1);
+
+                schema.read_number(*this, mstr);
+            }
+
+            // skip to end of field
+            pos = end_of_field;
+        }
+
+        pos = buffer.find_first_not_of(" \t\r\n", pos);
+    }
+
+    if (pos < buffer.size() && buffer[pos] == ']')
+    {
+        ++pos;
+    }
+
+    indent -= 4;
+}
--- src/gltf.cpp
+++ src/gltf.cpp
@@ -10,8 +10,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
-#include <vsg/io/json.h>
+//#include <vsg/io/json.h>
 
+#include "JSONParser.h"
 #include "gltf.h"
 
 #include <vsg/io/Path.h>
@@ -19,7 +20,275 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <fstream>
 
+using namespace vsg2;
 using namespace vsgXchange;
+
+template<typename T>
+struct container_schema : public vsg2::ArraySchema
+{
+    std::vector<T> values;
+    void read_number(JSONParser& parser, std::istream& input) override
+    {
+        T value;
+        input >> value;
+        values.push_back(value);
+    }
+};
+
+struct accessor_schema : public vsg2::ObjectSchema
+{
+    uint32_t bufferView = 0;
+    uint32_t byteOffset = 0;
+    uint32_t componentType = 0;
+    bool normalized = false;
+    uint32_t count = 0;
+    std::string type;
+    container_schema<double> max;
+    container_schema<double> min;
+
+    // sparse
+    // name
+    // extensions
+    // extras
+
+    void report()
+    {
+        vsg::info("accessor_schema { ");
+        vsg::info("    bufferView: ", bufferView);
+        vsg::info("    byteOffset: ", byteOffset);
+        vsg::info("    componentType: ", componentType);
+        vsg::info("    normalized: ", normalized);
+        vsg::info("    count: ", count);
+        vsg::info("    type: ", type);
+        for(auto& value : max.values) { vsg::info("    max : ", value); }
+        for(auto& value : min.values) { vsg::info("    min : ", value); }
+        vsg::info("} ");
+    }
+
+    void read_array(JSONParser& parser, const std::string_view& name) override
+    {
+        if (name == "max")
+        {
+            parser.read_array(max);
+        }
+        else if (name == "min")
+        {
+            parser.read_array(max);
+        }
+        else vsg::warn("A gltf parsing error, position = ", parser.pos);
+    }
+
+    void read_object(JSONParser& parser, const std::string_view& name) override
+    {
+        if (name == "sparse")
+        {
+        }
+        else if (name == "name")
+        {
+        }
+        else if (name == "extensions")
+        {
+        }
+        else if (name == "extras")
+        {
+        }
+        else vsg::warn("B gltf parsing error, position = ", parser.pos);
+    }
+
+    void read_string(JSONParser& parser, const std::string_view& name) override
+    {
+        if (name=="type" && parser.read_string(type)) {}
+        else vsg::warn("C gltf parsing error, position = ", parser.pos);
+    }
+
+    void read_number(JSONParser& parser, const std::string_view& name, std::istream& input) override
+    {
+        if (name=="bufferView") input >> bufferView;
+        else if (name=="byteOffset") input >> byteOffset;
+        else if (name=="componentType") input >> componentType;
+        else if (name=="count") input >> count;
+        else vsg::warn("D gltf parsing error, position = ", parser.pos, ", name = ", name);
+    }
+
+    void read_bool(JSONParser& parser, const std::string_view& name, bool value) override
+    {
+        if (name=="normalized") normalized = value;
+        else vsg::warn("E gltf parsing error, position = ", parser.pos);
+    }
+
+};
+
+struct accessors_schema : public vsg2::ArraySchema
+{
+    std::vector<accessor_schema> accessors;
+
+    void read_object(JSONParser& parser) override
+    {
+        vsg::info("accessors_schema::read_object()", this);
+
+        accessors.emplace_back();
+        parser.read_object(accessors.back());
+
+        vsg::info("done accessors_schema::read_object()", &accessors.back());
+        accessors.back().report();
+    }
+};
+
+
+struct glTF_schema : public vsg2::ObjectSchema
+{
+    accessors_schema accessors;
+
+    void read_array(JSONParser& parser, const std::string_view& name) override;
+    void read_object(JSONParser& parser, const std::string_view& name) override;
+    void read_string(JSONParser& parser, const std::string_view& name) override;
+    void read_number(JSONParser& parser, const std::string_view& name, std::istream& input) override;
+    void read_bool(JSONParser& parser, const std::string_view& name, bool value) override;
+    void read_null(JSONParser& parser, const std::string_view& name) override;
+};
+
+void glTF_schema::read_array(JSONParser& parser, const std::string_view& name)
+{
+    if (name == "extensionsUsed")
+    {
+        vsg::info("extensionsUsed schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "extensionsRequired")
+    {
+        vsg::info("extensionsRequired schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "accessors")
+    {
+        vsg::info("accessors schema required (",name,") ");
+        parser.read_array(accessors);
+    }
+    else if (name == "animations")
+    {
+        vsg::info("animations schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "buffers")
+    {
+        vsg::info("buffers schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "bufferViews")
+    {
+        vsg::info("bufferViews schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "cameras")
+    {
+        vsg::info("cameras schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "")
+    {
+        vsg::info(" schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "materials")
+    {
+        vsg::info("materials schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "meshes")
+    {
+        vsg::info("meshes schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "nodes")
+    {
+        vsg::info("nodes schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "samplers")
+    {
+        vsg::info("samplers schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "scenes")
+    {
+        vsg::info("scenes schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "skins")
+    {
+        vsg::info("skins schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else if (name == "textures")
+    {
+        vsg::info("textures schema required (",name,") ");
+        // parser.read_array(*this);
+    }
+    else
+    {
+        vsg::info("read_array(",name,") ");
+        //parser.read_array(*this);
+    }
+}
+
+void glTF_schema::read_object(JSONParser& parser, const std::string_view& name)
+{
+    if (name == "asset")
+    {
+        vsg::info("need to implement asset schema (",name,") ");
+        parser.read_object(*this);
+
+    }
+    else if (name == "scene")
+    {
+        vsg::info("need to implement scene schema (",name,") ");
+        parser.read_object(*this);
+    }
+    else if (name == "extensions")
+    {
+        vsg::info("need to implement extensions schema (",name,") ");
+        parser.read_object(*this);
+
+    }
+    else if (name == "extras")
+    {
+        vsg::info("need to implement extras schema (",name,") ");
+        parser.read_object(*this);
+    }
+    else
+    {
+        vsg::info("read_object(",name,") ");
+        parser.read_object(*this);
+    }
+}
+
+void glTF_schema::read_string(JSONParser& parser, const std::string_view& name)
+{
+    std::string str;
+    if (parser.read_string(str))
+    {
+        vsg::info("read_string(",name,") ", str);
+    }
+}
+
+void glTF_schema::read_number(JSONParser& parser, const std::string_view& name, std::istream& input)
+{
+    double number;
+    input >> number;
+
+    vsg::info("read_number(",name,") ", number);
+}
+
+void glTF_schema::read_bool(JSONParser& parser, const std::string_view& name, bool value)
+{
+    vsg::info("read_bool(",name,") ", value);
+}
+
+void glTF_schema::read_null(JSONParser& parser, const std::string_view& name)
+{
+    vsg::info("read_null(",name,")");
+}
+
 
 gltf::gltf()
 {
@@ -37,7 +306,9 @@ vsg::ref_ptr<vsg::Object> gltf::_read(std::istream& fin, vsg::ref_ptr<const vsg:
 
     if (fileSize==0) return {};
 
-    vsg::JSONParser parser;
+    vsg2::JSONParser parser;
+
+    glTF_schema schema;
 
     parser.buffer.resize(fileSize);
 
@@ -52,15 +323,11 @@ vsg::ref_ptr<vsg::Object> gltf::_read(std::istream& fin, vsg::ref_ptr<const vsg:
 
     if (parser.buffer[parser.pos]=='{')
     {
-        result =  parser.read_object();
-    }
-    else if (parser.buffer[parser.pos]=='[')
-    {
-        result =  parser.read_array();
+        parser.read_object(schema);
     }
     else
     {
-        vsg::info("Parsing error, could not find opening { or [.");
+        vsg::info("Parsing error, could not find opening {");
     }
 
     return result;
